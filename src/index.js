177531b301d9851390c3ee2eeb2936e1fa9794af
Haanieh Nasiraei Mir <haaniehnasiraeimir@gmail.com>
2025-02-07 16:31:10 +0330

    improved code

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ function generate(event) {
     let context =
       "you are a romantic poem expert who loves to say short poems. your mission is to generate a 4 line poem and separate each line with <br/>. make sure to follow the user instructions:";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    let poemElement = document.querySelector("#poem");
+    poemElement.style.display = 'block';
+    poemElement.innerHTML = `Generating the French poem about ${input.value}...`;
     axios.get(apiUrl).then(displayPoem);
     
     poemBox.innerHTML = "poem";
